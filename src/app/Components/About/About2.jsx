64f24f89fcadc30e4294da39885e4446b2949adb
Claudio Martinez <clamart85hs@gmail.com>
2025-10-23 23:59:04 +0200

    fix youtubevideo

--- src/app/Components/About/About2.jsx
+++ src/app/Components/About/About2.jsx
@@ -9,7 +9,7 @@ const About2 = () => {
         const [toggle, setToggle] = useState(false);
       
         const handelClick = () => {
-          setIframeSrc("https://www.youtube.com/watch?v=GcDXopVBN-k");
+          setIframeSrc("https://www.youtube.com/embed/GcDXopVBN-k");
           setToggle(!toggle);
         };
         const handelClose = () => {
